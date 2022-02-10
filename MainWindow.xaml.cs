ef14eb1580c6bdd497380dc75b9452bbd168d2b2
fictionalhero <95128388+fictionalhero@users.noreply.github.com>
2022-02-10 22:03:42 +0500

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Windows;
+using static GlazkiSave.Classes.Storage;
+using GlazkiSave.Pages;
+//чтобы открыть код, сочетание клавиш ctrl + M,  ctrl + L
+namespace GlazkiSave
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            try
+            {
+                frame = mainFrame;
+                frame.Navigate(new ViewingAgents());
+            }
+            catch (Exception ex)
+            {
+                ShowError(ex.ToString());
+            }
+        }
+
+        /// <summary>
+        /// Выход из приложения
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            MessageBoxResult res = MessageBox.Show("Вы уверены, что хотите выйти?", "Выход", MessageBoxButton.YesNo, MessageBoxImage.Information);
+            if (res == MessageBoxResult.No)
+                e.Cancel = true;
+        }
+    }
+}
+//flashforward
+//qqw//PIA//
